7364669eb81b842451b827d0216ebb0ac837011d
Keisuke Takagi <mit@takagi.app>
2023-04-22 03:11:49 +0900

    feat: add SideNavigationItem component

---
+++ apps/frontend/src/components/ui/SideNavigation/SideNavigationItem.stories.tsx
@@ -0,0 +1,34 @@
+import type { Meta, StoryObj } from "@storybook/react";
+
+import { SideNavigationItem } from "./SideNavigationItem";
+import { languages, templates } from "../../../utils/constants";
+
+const meta = {
+  title: 'ui/SideNavigationItem',
+  component: SideNavigationItem,
+  tags: ['autodocs'],
+  argTypes: {
+    onClick: { action: 'onClick' }
+  },
+} satisfies Meta<typeof SideNavigationItem>;
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+export const CurrentPath: Story = {
+  args: {
+    title: "Select Template",
+    description: "Express",
+    path: "/select-template",
+    isCurrent: true,
+  }
+}
+
+export const NotCurrentPath: Story = {
+  args: {
+    title: "Select Template",
+    description: "Code",
+    path: "/select-template",
+    isCurrent: false,
+  }
+}
