d631df91daee774f5d1957dabe6be395917fd69d
Keisuke Takagi <mit@takagi.app>
2023-04-24 00:41:41 +0900

    chore: add constants file

---
+++ apps/frontend/src/utils/constants.ts
@@ -0,0 +1,51 @@
+import type { Option } from "../types";
+
+export const templates: Option[] = [
+  {
+    text: 'Arc',
+    value: 'arc'
+  },
+  {
+    text: 'Cloudflare Pages',
+    value: 'cloudflare-pages'
+  },
+  {
+    text: 'Cloudflare Workers',
+    value: 'cloudflare-workers'
+  },
+  {
+    text: 'Deno',
+    value: 'deno'
+  },
+  {
+    text: 'Express',
+    value: 'express'
+  },
+  {
+    text: 'Fly',
+    value: 'fly'
+  },
+  {
+    text: 'Netlify',
+    value: 'netlify'
+  },
+  {
+    text: 'Remix',
+    value: 'remix'
+  },
+  {
+    text: 'Vercel',
+    value: 'vercel'
+  }
+]
+
+export const languages: Option[] = [
+  {
+    text: 'JavaScript',
+    value: 'javascript'
+  },
+  {
+    text: 'TypeScript',
+    value: 'typescript'
+  }
+]
